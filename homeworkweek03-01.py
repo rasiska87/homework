e4bd94c9b39ca98ade0797e4ffbd7e82efff9b04
rasiska87 <rasiska87@gmail.com>
2020-10-16 22:19:20 +0900

    3 week home work modify
    Pmongo Db Insert add

--- homeworkweek03-01.py
+++ homeworkweek03-01.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost',27017)
+db = client.sparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -14,4 +18,12 @@ for tr in trs:
     title = tr.select_one('td > a.title.ellipsis').text.strip()
     artist = tr.select_one('td > a.artist.ellipsis').text
     # print(rank, title, artist)
+    rankDoc = [
+        {
+            'rank':rank,
+            'title':title,
+            'artist':artist
+        }
+    ]
+    db.rank.insert(rankDoc)
     print(rank, title, artist)
